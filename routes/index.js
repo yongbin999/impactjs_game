6653c0a91430b56ce619673baad4deb6cadf4f56
yongbin999 <yongbinliang@umass.edu>
2016-01-08 10:13:59 -0500

    added bootstrap

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/credits', function(req, res, next) {
+  res.render('credits', { title: 'Express' });
+});
+
+router.get('/source', function(req, res, next) {
+  res.render('source', { title: 'Express' });
+});
+
+
+
 module.exports = router;
