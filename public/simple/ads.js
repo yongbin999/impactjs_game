1d0aefe1bea674b2fa8bcea4abc02d101587f8b5
yongbin999 <yongbinliang@umass.edu>
2016-01-17 21:47:40 -0500

    cleanup the ad format

--- public/simple/ads.js
+++ public/simple/ads.js
@@ -42,7 +42,7 @@ function requestAds() {
       onAdError,
       false);
 
-  // Request video ads.
+  // Request video ads. << put ur ad link here 
   var adsRequest = new google.ima.AdsRequest();
   adsRequest.adTagUrl = 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-games-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on';
 
